ad7d72e8175700cc132fb94c981cd9d511df57f9
Yazd <55390173+YazdKotwal-creator@users.noreply.github.com>
2020-05-09 15:18:29 +0530

    Add files via upload

---
+++ BookRequest.js
@@ -0,0 +1,11 @@
+import {Component}from 'react';
+import { Text, View } from 'react-native';
+export default class BookRequest extends Component{
+    render(){
+        return(
+        <View>
+            <Text>Book Request Screen</Text>
+        </View>
+        )
+    }
+}
